56cb6ed3fa0766aeca9ff23bc55cd7df0fce7c58
Shiv Prakash <iamshivprakash04@gmail.com>
2022-02-17 19:56:41 +0530

    first commit

---
+++ Expanse Tracker/public/firebase-credentials.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "<paste your apikey from firebase>",
+    authDomain: "<paste your authDomain from firebase>",
+    databaseURL: "<paste your url from firebase>",
+    projectId: "<paste your projectID from firebase>",
+    storageBucket: "<paste your storage bucket from firebase>",
+    messagingSenderId: "<paste your credentials>",
+    appId: "<paste your app Id>"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
